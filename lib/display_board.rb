3f7ebb21c3c3e96589e0c90d8c8a2543a35a224d
empathetic-parameter-3158 <nilagathp@gmail.com>
2019-04-13 17:36:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "{0} |   |   "
+  puts "[0] |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["x", " ", " ", " ", " ", " ", " ", " ", " "]
 puts #{board}
